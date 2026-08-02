94ecdb5f6850feec2971806cb52739a2ce438aa4
Disha <dishaggarwal03@gmail.com>
2026-08-02 16:02:30 +0530

    Fetch and display API data using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,54 @@
-import { useState } from "react";
-import Navbar from "./components/Navbar";
-import Product from "./components/Product";
-import Cart from "./components/Cart";
+import { useEffect, useState } from "react";
 
 function App() {
-const[cartItems, setCartItems] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
-function addToCart(product){
-  setCartItems((prevsItems) => [...prevsItems,product]);
-}
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch users");
+        }
+
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>Error: {error}</h2>;
+  }
 
   return (
     <>
-      <Navbar cartItems={cartItems}/>
+      <h1>Users List</h1>
 
-      <Product name="Laptop" onAddToCart={addToCart} />
-
-      <Product name="Mouse" onAddToCart={addToCart} />
-
-      <Product name="Keyboard" onAddToCart={addToCart} />
-
-      <Cart cartItems={cartItems} />
-
-      <p>Total Items: {cartItems.length}</p>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <strong>{user.name}</strong>
+            <br />
+            Email: {user.email}
+            <br />
+            Company: {user.company.name}
+            <br />
+            <br />
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
